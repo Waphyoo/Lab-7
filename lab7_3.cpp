7e6fb6301dced3def20e672471d3abfdd25ac717
non <nonny2246@gmail.com>
2022-12-13 16:05:49 +0700

    okok

--- lab7_3.cpp
+++ lab7_3.cpp
@@ -1,10 +1,27 @@
 #include<iostream>
 
 using namespace std;
+int adiff(int A,int B){
+  int c=0;
+  int a =A%360;
+  int b =B%360;
+  
+  if(a-b>=0){
+    c=a-b;
+  }else{
+    c=b-a;
+  }
+  if(c>180){
+    return 360-c;
+  }
+  return c;
+  
+  
+}
 
 
 int main(){
-  cout << adiff(180,270);
+  cout << adiff(180,270)<<"\n";
   cout << adiff(210,45);
   cout << adiff(0,360);
   cout << adiff(10,350);
